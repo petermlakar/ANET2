619121489681bda8f1dc5507f313866771d74885
Peter <peter.mlakar3@gmail.com>
2024-01-11 21:06:14 +0100

    Evaluation function qss implementation

--- feature_importance.py
+++ feature_importance.py
@@ -7,11 +7,12 @@ from os import mkdir, listdir
 
 import sys
 from dataset import Databank, Dataset, norm, normalize, denormalize, load_training_dataset, load_test_dataset 
-import netCDF4
 
 from time import time as timef
 
 import json
+
+from copy import deepcopy
 
 def crps(x, y):
 
@@ -43,128 +44,161 @@ CUDA = torch.cuda.is_available()
 
 #########################################################
 
-CFG_PATH = "" if len(sys.argv) == 1 else sys.argv[1]
+COMPUTE = False 
 
-cfg = json.load(open(join(CFG_PATH, "config.json")))
+if COMPUTE:
 
-OUTPUT_PATH = cfg["generateMulti"]["outputPath"] 
-MODELS_PATH = cfg["generateMulti"]["modelsPath"]
-BEST_MODEL_ONLY    = cfg["generateMulti"]["useBestModelOnly"] == "True"
-AVERAGE_PARAMETERS = cfg["generateMulti"]["averageParameters"] == "True"
-RESIDUALS = cfg["generateMulti"]["residuals"] == "True"
+    CFG_PATH = "" if len(sys.argv) == 1 else sys.argv[1]
 
-DATA_PATH = cfg["dataPath"]
+    cfg = json.load(open(join(CFG_PATH, "config.json")))
 
-if not exists(OUTPUT_PATH):
-    mkdir(OUTPUT_PATH)
+    OUTPUT_PATH = cfg["generateMulti"]["outputPath"] 
+    MODELS_PATH = cfg["generateMulti"]["modelsPath"]
+    BEST_MODEL_ONLY    = cfg["generateMulti"]["useBestModelOnly"] == "True"
+    AVERAGE_PARAMETERS = cfg["generateMulti"]["averageParameters"] == "True"
+    RESIDUALS = cfg["generateMulti"]["residuals"] == "True"
 
-print("Generating predictions with model:", MODELS_PATH.split("/")[-1], " -> using residuals: ", RESIDUALS)
+    DATA_PATH = cfg["dataPath"]
 
-#########################################################
-# Load training data, remove nan 
+    if not exists(OUTPUT_PATH):
+        mkdir(OUTPUT_PATH)
 
-_, _,\
-_, _,\
-_,\
-X_mean, X_std,\
-Y_mean, Y_std,\
-P_mean, P_std,\
-_, _,\
-_, _ = load_training_dataset(DATA_PATH, residuals = RESIDUALS)
+    print("Generating predictions with model:", MODELS_PATH.split("/")[-1], " -> using residuals: ", RESIDUALS)
 
-X, Y, P,\
-time, stations,\
-valid_time = load_test_dataset(DATA_PATH)
+    #########################################################
+    # Load training data, remove nan 
 
-X = normalize(X, X_mean, X_std)
-Y = normalize(Y, Y_mean, Y_std)
-P = normalize(P, P_mean, P_std)
+    _, _,\
+    _, _,\
+    _,\
+    X_mean, X_std,\
+    Y_mean, Y_std,\
+    P_mean, P_std,\
+    _, _,\
+    _, _ = load_training_dataset(DATA_PATH, residuals = RESIDUALS)
 
-#########################################################
+    X, Y, P,\
+    time, stations,\
+    valid_time = load_test_dataset(DATA_PATH)
 
-BATCH_SIZE = 512
+    X = normalize(X, X_mean, X_std)
+    P = normalize(P, P_mean, P_std)
 
-#########################################################
+    #########################################################
 
-models_losses = []
-models_regression  = []
-model_distribution = None
+    BATCH_SIZE = 512
 
-for m in listdir(MODELS_PATH):
+    #########################################################
 
-    models_regression.append(torch.jit.load(join(MODELS_PATH, m, "model_regression"), map_location = torch.device("cpu")))
-    models_losses.append(np.loadtxt(join(MODELS_PATH, m, "Valid_loss")).min())
-    model_distribution = model_distribution if model_distribution is not None else torch.jit.load(join(MODELS_PATH, m, "model_distribution"), map_location = torch.device("cpu"))
+    models_losses = []
+    models_regression  = []
+    model_distribution = None
+
+    for m in listdir(MODELS_PATH):
+
+        models_regression.append(torch.jit.load(join(MODELS_PATH, m, "model_regression"), map_location = torch.device("cpu")))
+        models_losses.append(np.loadtxt(join(MODELS_PATH, m, "Valid_loss")).min())
+        model_distribution = model_distribution if model_distribution is not None else torch.jit.load(join(MODELS_PATH, m, "model_distribution"), map_location = torch.device("cpu"))
+
+        if CUDA:
+            models_regression[-1] = models_regression[-1].to("cuda:0")
+
+        models_regression[-1].eval()
 
     if CUDA:
-        models_regression[-1] = models_regression[-1].to("cuda:0")
+        model_distribution = model_distribution.to("cuda:0")
 
-    models_regression[-1].eval()
+    #########################################################
 
-if CUDA:
-    model_distribution = model_distribution.to("cuda:0")
+    nbins = 51
 
-#########################################################
+    qstep = 1.0/(nbins + 1)
+    q = torch.arange(qstep, 1.0, step = qstep, dtype = torch.float32)
+    q = torch.reshape(q, (1, 1, q.shape[0]))
+    q = q.cuda() if CUDA else q
 
-nbins = 51
+    print(f"Number of quantiles: {q.shape} |  number of required parameters: 21x{model_distribution.number_of_outputs//21}")
 
-qstep = 1.0/(nbins + 1)
-q = torch.arange(qstep, 1.0, step = qstep, dtype = torch.float32)
-q = torch.reshape(q, (1, 1, q.shape[0]))
-q = q.cuda() if CUDA else q
+    #########################################################
 
-print(f"Number of quantiles: {q.shape} |  number of required parameters: 21x{model_distribution.number_of_outputs//21}")
+    def generate_scores(l, permutation, iX, iY, iP):
 
-#########################################################
+        scores = []
 
-def generate_scores(l, permutation, X, Y, P):
+        X = deepcopy(iX)
 
-    scores = []
+        with torch.no_grad():
 
-    with torch.no_grad():
+            shp = X.shape
 
-        # (229, 730, 21, 51) (229, 730, 21) (229, 4)
+            X = np.reshape(X, (np.prod(shp[:-2]), shp[-2], shp[-1]))
+            X[:, l] = X[permutation, l]
+            X = np.reshape(X, shp)
 
-        X = np.swapaxes(X, -1, -2)
-        shp = X.shape
-        X = np.reshape(X, (np.prod(shp[:-1]), shp[-1]))
-            
-        X[:, l] = X[permutation, l]
+            bank = Databank(X, iY, iP, valid_time, cuda = CUDA)
+            dataset = Dataset(bank, bank.index, batch_size = BATCH_SIZE, train = False, cuda = CUDA)
 
-        X = np.reshape(X, shp)
-        X = np.swapaxes(X, -1, -2)
+            for i in range(len(dataset)):
 
-        bank = Databank(X, Y, P, valid_time, cuda = CUDA)
-        dataset = Dataset(bank, bank.index, batch_size = BATCH_SIZE, train = False, cuda = CUDA)
+                x, p, y, j = dataset[i]
+                qtmp = q.expand(y.shape[0], y.shape[1], -1)
 
-        for i in range(len(dataset)):
+                f = []
+                for model in models_regression:
 
-            x, p, y, j = dataset[i]
-            qtmp = q.expand(y.shape[0], y.shape[1], -1)
+                    model_distribution.set_parameters(model(x, p))
+                    f.append(model_distribution.iF(qtmp))
 
-            f = []
-            for model in models_regression:
+                f = torch.stack(f, dim = 0).mean(dim = 0)
 
-                model_distribution.set_parameters(model(x, p))
-                f.append(model_distribution.iF(qtmp))
+                if RESIDUALS:
+                    f = (f*Y_std + (x*X_std + X_mean).mean(axis = -1)[..., None]).detach().cpu().numpy()
+                else:
+                    f = f.detach().cpu().numpy()*X_std + X_mean
 
-            f = torch.stack(f, dim = 0).mean(dim = 0)
+                y = y.detach().cpu().numpy()
 
-            if RESIDUALS:
-                f = (f*Y_std + (x*X_std + X_mean).mean(axis = -1)[..., None]).detach().cpu().numpy()
-            else:
-                f = f.detach().cpu().numpy()*X_std + X_mean
-            y = y.detach().cpu().numpy()
+                scores.append(crps(f, y))
 
-            scores.append(crps(f, y))
+                if (i + 1) % 100 == 0:
+                    print(f"    Computing scores on iteration {i + 1}/{len(dataset)}")
 
-            if (i + 1) % 100 == 0:
-                print(f"    Computing scores on iteration {i + 1}/{len(dataset)}")
+        return np.nanmean(np.concatenate(scores, axis = 0), axis = 0)
 
-    return np.stack(scores, axis = 0)
-            
-baseline = generate_scores(0, np.arange(8525670), X, Y, P)
+    #baseline = generate_scores(0, np.arange(167170), X, Y, P)
+    baseline = generate_scores(0, None, X, Y, P)
 
-print(baseline.shape)
+    I = np.zeros((21, 21), dtype = np.float32)
 
-# np.random.permutation(8525670)
+    for l in range(21):
+
+        print(f"\nEstimating relative importance of lead time {l + 1}")
+
+        scores = generate_scores(l, np.random.permutation(167170), X, Y, P)
+
+        I[:, l] = scores/baseline
+
+        print(f"Importance for lead time {l + 1} = " + "".join(list(map(lambda k: "{:.3f} ".format(k), I[:, l]))))
+
+        
+    np.save("importance", I)
+
+else:
+    import matplotlib.pyplot as plt
+
+    I = -np.flip(np.load("importance_relative.npy"), axis = 0)
+    #I = I/I.sum(axis = 0)[None]
+
+    print(I.mean(axis = 0))
+
+    for j in range(21):
+        for i in range(21):
+            print("{:.2f} ".format(I[j, i]), end = "")
+        print()
+
+    f, a = plt.subplots(1, dpi = 300, figsize = (10, 10))
+
+    a.imshow(I, cmap = "hot")
+    
+    f.tight_layout()
+    f.savefig("importance.pdf", format = "pdf", bbox_inches = "tight")
