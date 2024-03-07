d08d70669ab370ebf2b32bb550af68e9dff09408
Peter Mlakar <pm4824@hpc-login1.arnes.si>
2024-03-07 10:08:54 +0100

    Work

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ from os import mkdir
 import sys
 
 from torch.optim.lr_scheduler import ReduceLROnPlateau
-from dataset import Databank, Dataset, norm, normalize, load_training_dataset, denormalize
+from dataset import Databank, Dataset, load_training_dataset
 
 import json
 
